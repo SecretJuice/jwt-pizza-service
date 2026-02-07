55d76b84082dde947723e78a53955ad3e20cb167
Connor Robb <connor.m.robb33@gmail.com>
2026-02-06 17:37:14 -0700

    test: createAdminUser shared func

--- test/service.spec.js
+++ test/service.spec.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../src/service');
+const { Role, DB } = require('../src/database/database.js');
 
 // async function createAdminUser() {
 //   let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
@@ -12,12 +13,27 @@ const app = require('../src/service');
 //   return user;
 // }
 
+async function createAdminUser() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  await DB.addUser(user);
+  user.password = 'toomanysecrets';
+
+  return user;
+}
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12)
+}
+
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
 
 beforeAll(async () => {
-  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  testUser.email = randomName() + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
 });
@@ -34,5 +50,10 @@ test('login', async () => {
 
 test('bleh', async () => {
   expect(testUserAuthToken).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+})
+
+test('root', async () => {
+  const rootRes = await request(app).get('/').send()
+  expect(rootRes.body.message).toBe('welcome to JWT Pizza')
 })
 
