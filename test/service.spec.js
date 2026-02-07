f1afde5ba34e92c6d399b1b826f0ef3705d4afd7
Connor Robb <connor.m.robb33@gmail.com>
2026-02-06 18:08:16 -0700

    test: logout and register

--- test/service.spec.js
+++ test/service.spec.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../src/service');
+const version = require('../src/version.json');
 const { Role, DB } = require('../src/database/database.js');
 
 // async function createAdminUser() {
@@ -38,22 +39,54 @@ beforeAll(async () => {
   testUserAuthToken = registerRes.body.token;
 });
 
-test('login', async () => {
-  const loginRes = await request(app).put('/api/auth').send(testUser);
-  expect(loginRes.status).toBe(200);
-  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+describe('auth', () => {
+  test('login', async () => {
+    const loginRes = await request(app).put('/api/auth').send(testUser);
+    expect(loginRes.status).toBe(200);
+    expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 
-  const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
-  expect(loginRes.body.user).toMatchObject(user);
-  expect(password).toBe(password)
+    // eslint-disable-next-line no-unused-vars
+    const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+    expect(loginRes.body.user).toMatchObject(user);
+    expect(loginRes.body.user.password).toBeUndefined();
+  });
+
+  test('register', async () => {
+    const newUser = {
+      name: `user-${randomName()}`,
+      email: `${randomName()}@test.com`,
+      password: 'a',
+    };
+
+    const registerRes = await request(app).post('/api/auth').send(newUser);
+    expect(registerRes.status).toBe(200);
+    expect(registerRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+
+    const { password, ...expectedUser } = { ...newUser, roles: [{ role: 'diner' }] };
+    expect(registerRes.body.user).toMatchObject(expectedUser);
+    expect(registerRes.body.user.password).toBeUndefined();
+    expect(password).toBe(password);
+  });
+
+  test('logout', async () => {
+    const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
+    expect(logoutRes.status).toBe(200);
+    expect(logoutRes.body).toMatchObject({ message: 'logout successful' });
+
+    const secondLogoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
+    expect(secondLogoutRes.status).toBe(401);
+    expect(secondLogoutRes.body).toMatchObject({ message: 'unauthorized' });
+  });
 });
 
-test('bleh', async () => {
-  expect(testUserAuthToken).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
-})
+describe('base', () => {
+  test('root', async () => {
+    const rootRes = await request(app).get('/').send();
+    expect(rootRes.body.message).toBe('welcome to JWT Pizza');
+  });
 
-test('root', async () => {
-  const rootRes = await request(app).get('/').send()
-  expect(rootRes.body.message).toBe('welcome to JWT Pizza')
-})
-
+  test('docs', async () => {
+    const rootRes = await request(app).get('/api/docs').send();
+    expect(rootRes.body.version).toBe(version.version);
+  });
+});
