fff40c3c6882419aee9488f92ba7dc8e414342f6
Connor Robb <connor.m.robb33@gmail.com>
2026-04-07 22:04:18 -0600

    fix: sql injection + unprotected admin endpoint

--- src/database/database.js
+++ src/database/database.js
@@ -90,8 +90,8 @@ class DB {
         params.push(`name='${name}'`);
       }
       if (params.length > 0) {
-        const query = `UPDATE user SET ${params.join(', ')} WHERE id=${userId}`;
-        await this.query(connection, query);
+        const query = `UPDATE user SET password=?, email=?, name=? WHERE id=?`;
+        await this.query(connection, query, [...params, userId]);
       }
       return this.getUser(email, password);
     } finally {
