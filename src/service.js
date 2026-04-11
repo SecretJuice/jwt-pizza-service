fecf6d6178c56d299a421743b33d69494f17bc91
Connor Robb <connor.m.robb33@gmail.com>
2026-04-10 22:28:30 -0600

    fix: don't return stack traces on error

--- src/service.js
+++ src/service.js
@@ -49,7 +49,7 @@ app.use('*', (req, res) => {
 
 // Default error handler for all exceptions and errors.
 app.use((err, req, res, next) => {
-  res.status(err.statusCode ?? 500).json({ message: err.message, stack: err.stack });
+  res.status(err.statusCode ?? 500).json({ message: "Unexpected Error Occured. Try again later or contact us." });
   console.error(err, "Error Thrown")
   next();
 });
